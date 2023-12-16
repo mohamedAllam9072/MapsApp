3c6e2313f95c2ee9cfcc6bd71cb1cadfcd5c6e7f
mohamedallam <m.allam@sewedy.com.eg>
2023-12-16 18:19:30 +0200

    revamp

--- app/src/main/java/com/example/maps3/MapsActivity.java
+++ app/src/main/java/com/example/maps3/MapsActivity.java
@@ -1,28 +1,18 @@
 package com.example.maps3;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.DrawableRes;
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentActivity;
@@ -42,8 +32,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -58,126 +46,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        init();
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        binding.enableGPS.setOnClickListener(v -> {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                OnGPS();
+            } else {
+                x();
 
-        ActivityCompat.requestPermissions( this,
-                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        binding.enableGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    OnGPS();
-                } else {
-                    x();
-
-                }
             }
         });
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void init() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //  Log.d("TAG", "onMapReady:lat   " + currentLocation.getLatitude() + "     long" + currentLocation.getLongitude());
-        // Add a marker in Sydney and move the camera
-        //     LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 5f));
         getCurrentLocation();
-mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
-    @Override
-    public void onMapLoaded() {
-        LatLng customMarkerLocationOne = new LatLng(28.583911, 77.319116);
-        LatLng customMarkerLocationTwo = new LatLng(28.583078, 77.313744);
-        LatLng customMarkerLocationThree = new LatLng(28.580903, 77.317408);
-        LatLng customMarkerLocationFour = new LatLng(28.580108, 77.315271);
-        mMap.addMarker(new MarkerOptions().position(customMarkerLocationOne).
-                icon(BitmapDescriptorFactory.fromBitmap(
-                        createCustomMarker(MapsActivity.this,R.drawable.ic_launcher_background,"Manish")))).setTitle("iPragmatech Solutions Pvt Lmt");
-        mMap.addMarker(new MarkerOptions().position(customMarkerLocationTwo).
-                icon(BitmapDescriptorFactory.fromBitmap(
-                        createCustomMarker(MapsActivity.this,R.drawable.ic_launcher_background,"Narender")))).setTitle("Hotel Nirulas Noida");
-
-        mMap.addMarker(new MarkerOptions().position(customMarkerLocationThree).
-                icon(BitmapDescriptorFactory.fromBitmap(
-                        createCustomMarker(MapsActivity.this,R.drawable.ic_launcher_background,"Neha")))).setTitle("Acha Khao Acha Khilao");
-        mMap.addMarker(new MarkerOptions().position(customMarkerLocationFour).
-                icon(BitmapDescriptorFactory.fromBitmap(
-                        createCustomMarker(MapsActivity.this,R.drawable.ic_launcher_background,"Nupur")))).setTitle("Subway Sector 16 Noida");
-        //LatLngBound will cover all your marker on Google Maps
-        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        builder.include(customMarkerLocationOne); //Taking Point A (First LatLng)
-        builder.include(customMarkerLocationThree); //Taking Point B (Second LatLng)
-        LatLngBounds bounds = builder.build();
-        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 200);
-        mMap.moveCamera(cu);
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
-
-    }
-});
+        mMap.setOnMapLoadedCallback(() -> {
+            LatLng customMarkerLocationOne = new LatLng(28.583911, 77.319116);
+            LatLng customMarkerLocationTwo = new LatLng(28.583078, 77.313744);
+            LatLng customMarkerLocationThree = new LatLng(28.580903, 77.317408);
+            LatLng customMarkerLocationFour = new LatLng(28.580108, 77.315271);
+            mMap.addMarker(new MarkerOptions().position(customMarkerLocationOne).icon(BitmapDescriptorFactory.fromBitmap(Utils.createCustomMarker(MapsActivity.this, R.drawable.ic_launcher_background, "Manish")))).setTitle("iPragmatech Solutions Pvt Lmt");
+            mMap.addMarker(new MarkerOptions().position(customMarkerLocationTwo).icon(BitmapDescriptorFactory.fromBitmap(Utils.createCustomMarker(MapsActivity.this, R.drawable.ic_launcher_background, "Narender")))).setTitle("Hotel Nirulas Noida");
+            mMap.addMarker(new MarkerOptions().position(customMarkerLocationThree).icon(BitmapDescriptorFactory.fromBitmap(Utils.createCustomMarker(MapsActivity.this, R.drawable.ic_launcher_background, "Neha")))).setTitle("Acha Khao Acha Khilao");
+            mMap.addMarker(new MarkerOptions().position(customMarkerLocationFour).icon(BitmapDescriptorFactory.fromBitmap(Utils.createCustomMarker(MapsActivity.this, R.drawable.ic_launcher_background, "Nupur")))).setTitle("Subway Sector 16 Noida");
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(customMarkerLocationOne);
+            builder.include(customMarkerLocationThree);
+            LatLngBounds bounds = builder.build();
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 200);
+            mMap.moveCamera(cu);
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+        });
 
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         mMap.setMyLocationEnabled(true);
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title("mohamed").snippet("allam"));
-                Toast.makeText(MapsActivity.this, latLng.latitude + "\n" + latLng.longitude, Toast.LENGTH_SHORT).show();
-            }
+        mMap.setOnMapClickListener(latLng -> {
+            mMap.addMarker(new MarkerOptions().position(latLng).title("mohamed").snippet("allam"));
+            Toast.makeText(MapsActivity.this, latLng.latitude + "\n" + latLng.longitude, Toast.LENGTH_SHORT).show();
         });
     }
 
     private void getCurrentLocation() {
         mFused = LocationServices.getFusedLocationProviderClient(this);
         try {
-            Task location = mFused.getLastLocation();
-            location.addOnCompleteListener(new OnCompleteListener() {
-                @Override
-                public void onComplete(@NonNull Task task) {
-                    if (task.isSuccessful()) {
-                        Log.d("bbb", "onComplete: success");
-                        Location c_Location = (Location) task.getResult();
-                   //     m_moveCamera(new LatLng(c_Location.getLatitude(), c_Location.getLongitude()), 15f);
-                    } else {
-                        Log.d("bbb", "onComplete: failed");
+            Task<Location> location = mFused.getLastLocation();
+            location.addOnCompleteListener((OnCompleteListener<Location>) task -> {
+                if (task.isSuccessful()) {
+                    Location location1 = (Location) task.getResult();
+                    if (location1 != null) {
+                        movingCamera(new LatLng(location1.getLatitude(), location1.getLongitude()), 15f);
                     }
                 }
             });
 
 
         } catch (SecurityException e) {
-            Log.e("bbb", "security exception " + e.getMessage());
+            e.fillInStackTrace();
         }
     }
 
-    private void m_moveCamera(LatLng latLng, float zoom) {
+    private void movingCamera(LatLng latLng, float zoom) {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
@@ -191,16 +138,17 @@ mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
             if (locationGPS != null) {
                 double lat = locationGPS.getLatitude();
                 double lng = locationGPS.getLongitude();
-                m_moveCamera(new LatLng(lat,lng),15f);
+                movingCamera(new LatLng(lat, lng), 15f);
                 Log.d("bbb", "x: " + "lat " + lat + "  lng  " + lng);
             } else {
                 Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
     private void OnGPS() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
@@ -214,25 +162,6 @@ mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
         final AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
-    public static Bitmap createCustomMarker(Context context, @DrawableRes int resource, String _name) {
 
-        View marker = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_map_marker, null);
 
-        CircleImageView markerImage = marker.findViewById(R.id.user_dp);
-        markerImage.setImageResource(resource);
-        TextView txt_name =marker.findViewById(R.id.name);
-        txt_name.setText(_name);
-
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        marker.setLayoutParams(new ViewGroup.LayoutParams(52, ViewGroup.LayoutParams.WRAP_CONTENT));
-        marker.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
-        marker.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
-        marker.buildDrawingCache();
-        Bitmap bitmap = Bitmap.createBitmap(marker.getMeasuredWidth(), marker.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        marker.draw(canvas);
-
-        return bitmap;
-    }
 }
